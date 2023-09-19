a6b5fcd7a98aebe19fb207d124c72bfef2a52472
parmod <parmodmagotra6391@gmail.com>
2023-09-19 12:01:35 +0530

    Added create booking repository code

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,30 @@
+const { Booking } = require("../models/index");
+
+const {
+  AppError,
+  ValidationError,
+  ServiceError,
+} = require("../utils/errors/index");
+
+const { StatusCodes } = require("http-status-codes");
+
+class BookingRepository {
+  async create(data) {
+    try {
+      const booking = await Booking.create(data);
+      return booking;
+    } catch (error) {
+      if (error.name == "SequelizeValidationError") {
+        throw new ValidationError(error);
+      }
+      throw new AppError(
+        "RepositoryError",
+        "Cannot create booking",
+        "There was some issue creating the booking , try again later",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+}
+
+module.exports = BookingRepository;
